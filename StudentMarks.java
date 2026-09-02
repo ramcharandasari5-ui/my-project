6240cc0b44c42c9a2b492099a6feb5511d0e885d
Dasari Ramcharan <ramcharandasari5@gmail.com>
2026-09-02 17:13:26 +0530

    Added StudentMarks program to search for a target mark

---
+++ StudentMarks.java
@@ -0,0 +1,23 @@
+public class StudentMarks
+{
+    public static boolean SearchMark(int[] marks,int targermark)
+    {
+        boolean found = false;
+        for(int i=0;i<marks.length;i++)
+        {
+            if(marks[i] == targermark)
+            {
+                found = true;
+                break;
+            }
+        }
+    return found;
+    }
+    public static void main(String[] args)
+    {
+        int[] marks = {85,44,62,80,92};
+        int targermark = 80;
+        int result = SearchMark(marks,targermark);
+        System.out.println(result);y
+    }
+} 
